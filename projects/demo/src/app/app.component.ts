1bcae0b1b354d7e023e05e818021a04f21c2c3bb
hhfrancois <francois.achache@gmail.com>
2025-12-11 06:04:56 +0100

    feat: Implement lazy loading for playground and documentation components

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { PlaygroundComponent } from './playground/playground.component';
-import { DocumentationComponent } from './documentation/documentation.component';
 
 @Component({
   selector: 'app-root',
   imports: [
+    RouterOutlet,
     MatIconModule,
-    PlaygroundComponent,
-    DocumentationComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
